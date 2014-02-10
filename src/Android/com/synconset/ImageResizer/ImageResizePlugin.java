1885ba980cd388106729a8c7d102a88fb7d7afcb
Andrew Stephan <thekiteeatingtree@gmail.com>
2014-02-10 11:45:11 -0500

    android now works but broke ios for now

--- src/Android/com/synconset/ImageResizer/ImageResizePlugin.java
+++ src/Android/com/synconset/ImageResizer/ImageResizePlugin.java
@@ -59,9 +59,8 @@ public class ImageResizePlugin extends CordovaPlugin {
 		String imageDataType;
 		String format;
 		Bitmap bmp;
-		Log.d("PLUGIN", action);
 		try {
-			// parameters (forst object of the json array)
+			// parameters (first object of the json array)
 			params = data.getJSONObject(0);
 			// image data, either base64 or url
 			imageData = params.getString("data");
@@ -73,6 +72,17 @@ public class ImageResizePlugin extends CordovaPlugin {
 					: DEFAULT_FORMAT;
 			// create the Bitmap object, needed for all functions
 			bmp = getBitmap(imageData, imageDataType);
+			
+			if (action.equals("resizeImage")) {
+    			return resizeImage(params, format, bmp, callbackContext);
+    		} else if (action.equals("imageSize")) {
+    			return getImageSize(bmp, callbackContext);
+    		} else if (action.equals("storeImage")) {
+    			return storeImage(params, format, bmp, callbackContext);
+    		} else {
+    		    Log.d("PLUGIN", "unknown action");
+    		    return false;
+    		}
 		} catch (JSONException e) {
 			Log.d("PLUGIN", e.getMessage());
 			callbackContext.error(e.getMessage());
@@ -86,183 +96,126 @@ public class ImageResizePlugin extends CordovaPlugin {
 			callbackContext.error(e.getMessage());
 			return false;
 		}
-		// resize the image
-		Log.d("PLUGIN", "passed init");
-		if (action.equals("resizeImage")) {
-			return resizeImage(params, format, bmp, callbackContext);
-		} else if (action.equals("imageSize")) {
-			try {
-				JSONObject res = new JSONObject();
-				res.put("width", bmp.getWidth());
-				res.put("height", bmp.getHeight());
-				Log.d("PLUGIN", "finished get image size");
-				callbackContext.success(res);
-				return true;
-			} catch (JSONException e) {
-				callbackContext.error(e.getMessage());
-				return false;
-			}
-		} else if (action.equals("storeImage")) {
-			return storeImage(params, format, bmp, callbackContext);
-		}
-		Log.d("PLUGIN", "unknown action");
-		return false;
 	}
 	
-	private boolean resizeImage(JSONObject params, String format, Bitmap bmp, CallbackContext callbackContext) {
+	private boolean getImageSize(Bitmap bmp, CallbackContext callbackContext) {
 	    try {
-			double widthFactor;
-			double heightFactor;
-
-			// compression quality
-			int quality = params.getInt("quality");
-
-			// Pixels or Factor resize
-			String resizeType = params.getString("resizeType");
-
-			// Get width and height parameters
-			double width = params.getDouble("width");
-			double height = params.getDouble("height");
-
-			if (resizeType.equals(RESIZE_TYPE_MIN_PIXEL)) {
-				widthFactor = width / ((double) bmp.getWidth());
-				heightFactor = height / ((double) bmp.getHeight());
-				if (widthFactor > heightFactor && widthFactor <= 1.0) {
-		            heightFactor = widthFactor;
-				} else if (heightFactor <= 1.0) {
-				    widthFactor = heightFactor;
-				} else {
-				    widthFactor = 1.0;
-				    heightFactor = 1.0;
-				}
-			} else if (resizeType.equals(RESIZE_TYPE_MAX_PIXEL)) {
-			    widthFactor = width / ((double) bmp.getWidth());
-				heightFactor = height / ((double) bmp.getHeight());
-				if (widthFactor == 0.0) {
-    	            widthFactor = heightFactor;
-                } else if (heightFactor == 0.0) {
-                	heightFactor = widthFactor;
-                } else if (widthFactor > heightFactor) {
-                    widthFactor = heightFactor; // scale to fit height
-                } else {
-                    heightFactor = widthFactor; // scale to fit width
-                }
-			} else {
-				widthFactor = width;
-				heightFactor = height;
-			}
-			
-			if (params.getBoolean("pixelDensity")) {
-			    DisplayMetrics metrics = cordova.getActivity().getResources().getDisplayMetrics();
-			    if (metrics.density > 1) {
-			        if (widthFactor * metrics.density < 1.0 && heightFactor * metrics.density < 1.0) {
-			            widthFactor *= metrics.density;
-			            heightFactor *= metrics.density;
-			        } else {
-			            widthFactor = 1.0;
-			            heightFactor = 1.0;
-			        }
-			    }
-			}
-
-			Bitmap resized = getResizedBitmap(bmp, (float) widthFactor,
-					(float) heightFactor);
-					
-			if (params.getBoolean("storeImage")) {
-			    try {
-    			    String filename = params.getString("filename");
-    			    Log.d("PLUGIN", "storing image");
-    			    Log.d("PLUGIN", params.getString("directory"));
-    			    URI uri = new URI(params.getString("directory") + "/" + filename);
-    			    Log.d("PLUGIN", uri.toString());
-    			    File file = new File(uri);
-    			    file.mkdirs();
-    			    OutputStream outStream = new FileOutputStream(file);
-    				if (format.equals(FORMAT_PNG)) {
-    					resized.compress(Bitmap.CompressFormat.PNG, quality,
-    							outStream);
-    				} else {
-    					resized.compress(Bitmap.CompressFormat.JPEG, quality,
-    							outStream);
-    				}
-    				outStream.flush();
-    				outStream.close();
-    				JSONObject res = new JSONObject();
-    				res.put("filename", filename);
-    				res.put("width", resized.getWidth());
-    				res.put("height", resized.getHeight());
-    				callbackContext.success(res);
-			    } catch(IOException e) {
-			        Log.d("PLUGIN", e.getMessage());
-			        callbackContext.error(e.getMessage());
-			        return false;
-			    } catch(URISyntaxException e) {
-			    	Log.d("PLUGIN", e.getMessage());
-			        callbackContext.error(e.getMessage());
-			        return false;
-			    }
-			} else {
-			    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				if (format.equals(FORMAT_PNG)) {
-					resized.compress(Bitmap.CompressFormat.PNG, quality, baos);
-				} else {
-					resized.compress(Bitmap.CompressFormat.JPEG, quality, baos);
-				}
-				byte[] b = baos.toByteArray();
-				String returnString = Base64.encodeToString(b, Base64.DEFAULT);
-				// return object
-				JSONObject res = new JSONObject();
-				res.put("imageData", returnString);
-				res.put("width", resized.getWidth());
-				res.put("height", resized.getHeight());
-				callbackContext.success(res);
-			}
+			JSONObject res = new JSONObject();
+			res.put("width", bmp.getWidth());
+			res.put("height", bmp.getHeight());
+			callbackContext.success(res);
 			return true;
 		} catch (JSONException e) {
-		    Log.d("PLUGIN", e.getMessage());
 			callbackContext.error(e.getMessage());
 			return false;
 		}
 	}
 	
-	private boolean storeImage(JSONObject params, String format, Bitmap bmp, CallbackContext callbackContext) {
-	    try {
-			// Obligatory Parameters, throw JSONException if not found
-			String filename = params.getString("filename");
-			filename = (filename.contains(".")) ? filename : filename + "."
-					+ format;
-			String directory = params.getString("directory");
-			directory = directory.startsWith("/") ? directory : "/"
-					+ directory;
-			int quality = params.getInt("quality");
+	private boolean resizeImage(JSONObject params, String format, Bitmap bmp, 
+	        CallbackContext callbackContext) throws JSONException, IOException, 
+	        URISyntaxException {
+		double widthFactor;
+		double heightFactor;
 
-			OutputStream outStream;
-			// store the file locally using the external storage directory
-			File file = new File(Environment.getExternalStorageDirectory()
-					.toString() + directory, filename);
-			try {
-				outStream = new FileOutputStream(file);
-				if (format.equals(FORMAT_PNG)) {
-					bmp.compress(Bitmap.CompressFormat.PNG, quality,
-							outStream);
-				} else {
-					bmp.compress(Bitmap.CompressFormat.JPEG, quality,
-							outStream);
-				}
-				outStream.flush();
-				outStream.close();
-				JSONObject res = new JSONObject();
-				res.put("url", "file://" + file.getAbsolutePath());
-				callbackContext.success(res);
-				return true;
-			} catch (IOException e) {
-				callbackContext.error(e.getMessage());
-				return false;
+		// Pixels or Factor resize
+		String resizeType = params.getString("resizeType");
+
+		// Get width and height parameters
+		double width = params.getDouble("width");
+		double height = params.getDouble("height");
+
+		if (resizeType.equals(RESIZE_TYPE_MIN_PIXEL)) {
+			widthFactor = width / ((double) bmp.getWidth());
+			heightFactor = height / ((double) bmp.getHeight());
+			if (widthFactor > heightFactor && widthFactor <= 1.0) {
+	            heightFactor = widthFactor;
+			} else if (heightFactor <= 1.0) {
+			    widthFactor = heightFactor;
+			} else {
+			    widthFactor = 1.0;
+			    heightFactor = 1.0;
 			}
-		} catch (JSONException e) {
-			callbackContext.error(e.getMessage());
-			return false;
+		} else if (resizeType.equals(RESIZE_TYPE_MAX_PIXEL)) {
+		    widthFactor = width / ((double) bmp.getWidth());
+			heightFactor = height / ((double) bmp.getHeight());
+			if (widthFactor == 0.0) {
+	            widthFactor = heightFactor;
+            } else if (heightFactor == 0.0) {
+            	heightFactor = widthFactor;
+            } else if (widthFactor > heightFactor) {
+                widthFactor = heightFactor; // scale to fit height
+            } else {
+                heightFactor = widthFactor; // scale to fit width
+            }
+		} else {
+			widthFactor = width;
+			heightFactor = height;
 		}
+		
+		if (params.getBoolean("pixelDensity")) {
+		    DisplayMetrics metrics = cordova.getActivity().getResources().getDisplayMetrics();
+		    if (metrics.density > 1) {
+		        if (widthFactor * metrics.density < 1.0 && heightFactor * metrics.density < 1.0) {
+		            widthFactor *= metrics.density;
+		            heightFactor *= metrics.density;
+		        } else {
+		            widthFactor = 1.0;
+		            heightFactor = 1.0;
+		        }
+		    }
+		}
+
+		Bitmap resized = getResizedBitmap(bmp, (float) widthFactor,
+				(float) heightFactor);
+				
+		if (params.getBoolean("storeImage")) {
+		    return storeImage(params, format, resized, callbackContext);
+		} else {
+		    int quality = params.getInt("quality");
+		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			if (format.equals(FORMAT_PNG)) {
+				resized.compress(Bitmap.CompressFormat.PNG, quality, baos);
+			} else {
+				resized.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+			}
+			byte[] b = baos.toByteArray();
+			String returnString = Base64.encodeToString(b, Base64.DEFAULT);
+			// return object
+			JSONObject res = new JSONObject();
+			res.put("imageData", returnString);
+			res.put("width", resized.getWidth());
+			res.put("height", resized.getHeight());
+			callbackContext.success(res);
+		}
+		return true;
+	}
+	
+	private boolean storeImage(JSONObject params, String format, Bitmap bmp, 
+	        CallbackContext callbackContext) throws JSONException, IOException, 
+	        URISyntaxException {
+	    int quality = params.getInt("quality");
+	    String filename = params.getString("filename");
+	    URI folderUri = new URI(params.getString("directory"));
+	    URI pictureUri = new URI(params.getString("directory") + "/" + filename);
+	    File folder = new File(folderUri);
+	    folder.mkdirs();
+	    File file = new File(pictureUri);
+	    OutputStream outStream = new FileOutputStream(file);
+		if (format.equals(FORMAT_PNG)) {
+			bmp.compress(Bitmap.CompressFormat.PNG, quality,
+					outStream);
+		} else {
+			bmp.compress(Bitmap.CompressFormat.JPEG, quality,
+					outStream);
+		}
+		outStream.flush();
+		outStream.close();
+		JSONObject res = new JSONObject();
+		res.put("filename", filename);
+		res.put("width", bmp.getWidth());
+		res.put("height", bmp.getHeight());
+		callbackContext.success(res);
+		return true;
 	}
 
 	private Bitmap getResizedBitmap(Bitmap bm, float widthFactor,
@@ -286,10 +239,8 @@ public class ImageResizePlugin extends CordovaPlugin {
 			byte[] blob = Base64.decode(imageData, Base64.DEFAULT);
 			bmp = BitmapFactory.decodeByteArray(blob, 0, blob.length);
 		} else {
-		    //File imageFile = new File(cordova.getActivity().getFilesDir(), imageData);
 		    URI uri = new URI(imageData);
 		    File imageFile = new File(uri);
-		    Log.d("PLUGIN", imageFile.getAbsolutePath());
 		    bmp = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
 		    if (bmp == null) {
 		        throw new IOException("The image file could not be opened.");
